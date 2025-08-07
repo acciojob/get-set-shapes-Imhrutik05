0ca9d3cd632d045313aefd05f44dc0cd178d4ced
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-07 19:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this._width = width;
+		this._height = height;
+	}
+	getArea(){
+		return this._width * this._height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(ide){
+		super(side, side);
+	}
+	getPerimeter(){
+		return this._side * 4;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
